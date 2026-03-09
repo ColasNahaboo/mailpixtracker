91ba3674a90fefd048726e391651d2ce6d87d637
Colas Nahaboo <colas@nahaboo.net>
2026-03-09 09:26:26 +0100

    backcompat with v1 log format

---
+++ doc/random-string.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# random-string.sh [N]
+# prints  a random alphanumeric string of N chars (default 8)
+
+random_string() { # LC_ALL=C avoids unicode multibyte mess
+    LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/urandom 2>/dev/null | head -c "${1:-12}"
+}
+
+n="${1//[^[0-9]/}"
+(( n < 1 )) && n=8
+random_string "$n"
+echo
